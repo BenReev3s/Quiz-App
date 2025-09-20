3a6f7c745defa6fe37cdad176706c97740a11ce6
Ben reeves <72570746+UP957029@users.noreply.github.com>
2025-09-20 22:51:58 +0100

    Initial commit - database js file set up

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./quiz.db');
+
+db.serialize(() => {
+    db.run(`
+       CREATE TABLE IF NOT EXISTS questions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question TEXT NOT NULL,
+        correct_answer TEXT NOT NULL
+       ) 
+        `)
+    db.run(`
+        CREATE TABLE IF NOT EXISTS scores (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            score INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT NOT NULL
+        )
+        `);
+});
+
+module.exports = db;
