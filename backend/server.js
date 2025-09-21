809180221fcac2f7fa6aa28739fc39016b4dd9b6
Ben reeves <72570746+UP957029@users.noreply.github.com>
2025-09-21 09:38:28 +0100

    Server created, three routes. Route 1 recieves the question row, route 2 compares the users answer to the correct_answer and rote 3 recives leaderboard

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const db = require('./db')
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json());
+
+app.get('/questions', (red, res) => {
+    db.get(
+        `SELECT * FROM questions ORDER BY RANDOM() LIMIT 1`,
+        (err, row) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json(row)
+        }
+    );
+});
+
+app.post('/submit', (res, req) => {
+    const { username, questionId, answer } = req.body;
+    db.get(
+        `SELECT correct_answer FROM questions WHERE id = ?`,
+        [questionId],
+        (err, row) => {
+            if (err) {
+                res.status(500).json({ error: err.message })
+                return;
+            };
+            const isCorrect = row && row.correct_answer.toLowerCase() === answer.toLowerCase()
+
+            if (isCorrect) {
+                db.run(
+                    `INSERT INTO scores (username, score) VLAUES (?, 1)`,
+                    [username],
+                    function (err) {
+                        if (err) {
+                            res.status(500).json({ error: err.message });
+                            return;
+                        }
+                        res.json({ correct: true, scoreId: this.lastID })
+                    }
+                );
+            } else {
+                res.json({ correct: false })
+            }
+
+        }
+    );
+});
+
+app.get('./leaderboard', (res, req) => {
+    db.all(
+        `SELECT username, SUM(score), as total_score FROM scores GROUP BY username ORDER by total_score DESC LIMIT 10`,
+        (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message })
+                return;
+            }
+            res.json(rows)
+        }
+    );
+});
+
+app.listen(PROTOCOL, () => {
+    console.log(`server running at http://localhost${port}`);
+});
